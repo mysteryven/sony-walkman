1d7d0fe1a01a15e99f2d8ba7aa3b2416a4447e58
wenzhe <mysteryven@gmail.com>
2018-10-12 10:14:55 +0800

    fix clock bug

--- src/js/clock.js
+++ src/js/clock.js
@@ -3,13 +3,12 @@
        let data = new Date()
        let hour = data.getHours()
        let minute = data.getMinutes()
-       let second = data.getSeconds()
        let session = hour < 13 ? 'AM' : 'PM'
 
+       minute = minute < 10 ? '0' + minute : minute
        let current = `${hour}:${minute} ${session}`
        $('#timer').html(current)
     }
 
     setInterval(showTime, 1000)
-
 }
